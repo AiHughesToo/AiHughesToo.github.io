02a62a8e2725576ac0a4292c0d99c3d097e795cf
AiHughesToo <alex.hughes@interapthq.com>
2016-09-28 00:39:24 -0400

    n

---
+++ tipcal.js
@@ -0,0 +1,35 @@
+// set the global variables
+var billAmount = 0;
+var tipMult = 0;
+var numPeople = 0;
+
+
+// function to calculate the tip
+function calTip(info) {
+console.log("im at calTip");
+tipDiv(1);
+};
+
+
+
+
+//function to divide the tip and bill
+
+function tipDiv (tipAmount){
+console.log("im at the tip amount" + tipAmount);
+
+};
+
+
+
+// function to start it off
+// this will be on a click event fo the calculate button
+function  start(){
+
+    
+};
+
+
+
+
+//function to send to the page
